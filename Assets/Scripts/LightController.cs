f5848bb6c05f108ae1b2ba52531b0ad79fea2b88
Onist Der Falke <75864407+OnistDerFalke@users.noreply.github.com>
2022-10-07 00:39:03 +0200

    Baterie, poprawiony skok, animacje

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Microsoft.Win32.SafeHandles;
 using UnityEngine;
 
 public class LightController : MonoBehaviour
@@ -17,40 +14,39 @@ public class LightController : MonoBehaviour
     {
         var rot = gameObject.transform.localEulerAngles;
         
-        if (Input.GetKey(KeyCode.RightArrow) && rot.z <= 180)
+        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && rot.z <= 180)
             rot.z = 360-rot.z;
-        if(Input.GetKey(KeyCode.LeftArrow) && rot.z >= 180)
+        if((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && rot.z >= 180)
             rot.z = 360-rot.z;
         
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
             if (rot.z >= lightLimesUp + 180 && rot.z <= lightLimesDown + 180)
             {
-                rot.z += lightSpeed;
+                rot.z += lightSpeed * Time.deltaTime;
                 if (rot.z > lightLimesDown + 180) rot.z = lightLimesDown + 180;
             }
             else if(rot.z <= lightLimesDown && rot.z >= lightLimesUp)
             {
-                rot.z -= lightSpeed;
+                rot.z -= lightSpeed * Time.deltaTime;
                 if (rot.z < lightLimesUp) rot.z = lightLimesUp;
             }
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             if (rot.z >= lightLimesUp + 180 && rot.z <= lightLimesDown + 180)
             {
-                rot.z -= lightSpeed;
+                rot.z -= lightSpeed * Time.deltaTime;
                 if (rot.z < lightLimesUp + 180) rot.z = lightLimesUp + 180;
             }
             else if(rot.z <= lightLimesDown && rot.z >= lightLimesUp)
             {
-                rot.z += lightSpeed;
+                rot.z += lightSpeed * Time.deltaTime;
                 if (rot.z > lightLimesDown) rot.z = lightLimesDown;
             }
         }
 
         rot.z %= 360;
-        Debug.Log(rot);
         gameObject.transform.localEulerAngles = rot;
     }
 }
--- Assets/Scripts/PlayerControllerLevel1.cs
+++ Assets/Scripts/PlayerControllerLevel1.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 
 public class PlayerControllerLevel1 : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer rend;
+    
     public float moveSpeed = 0.1f;
     public float jumpForce = 6f;
 
+    public LayerMask groundLayer;
+
     private Rigidbody2D rigidBody;
     
-    void Start()
-    {
-        
-    }
-
+    private static readonly int MovementSpeed = Animator.StringToHash("MovementSpeed");
+    private static readonly int IsInJump = Animator.StringToHash("IsInJump");
+    private float currentSpeed;
+    private bool stillOnPlatform;
+    private int score;
+    
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -22,14 +28,77 @@ public class PlayerControllerLevel1 : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Translate(moveSpeed * Time.deltaTime, 0, 0, Space.World);
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0, Space.World);
-
-        if (Input.GetMouseButtonDown(0))
+        currentSpeed = 0;
+        FlipSpriteOnMove();
+        
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            transform.Translate(moveSpeed * Time.deltaTime, 0, 0, Space.World);
+            currentSpeed = moveSpeed * Time.deltaTime;
         }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0, Space.World);
+            currentSpeed = moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        
+        animator.SetFloat(MovementSpeed, currentSpeed);
+        animator.SetBool(IsInJump, !IsGrounded());
+    }
+
+    private bool IsGrounded()
+    {
+        var hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, groundLayer.value);
+        if (stillOnPlatform) return true;
+        return hit.collider != null;
+    }
+    private void Jump()
+    {
+        if (!IsGrounded()) return;
+        rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Platform"))
+        {
+            stillOnPlatform = true;
+        }
+    }
+    
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Platform"))
+        {
+            stillOnPlatform = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Battery"))
+        {
+            score += 1;
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.CompareTag("Portal"))
+        {
+            Debug.Log("Portal trigger");
+        }
+    }
+    
+    private void FlipSpriteOnMove()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            rend.flipX = true;
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            rend.flipX = false;
     }
 }
